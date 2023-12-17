49a24d24db517860e1ae7ae840c11deb2a9325b8
Tim <tima4056@gmail.com>
2023-12-18 01:03:07 +0300

    Some config changes

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=petsy',
+    'dsn' => 'mysql:host=127.0.0.1:3306;dbname=petsy', //localhost
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
